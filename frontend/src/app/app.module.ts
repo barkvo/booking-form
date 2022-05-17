3b9e3d5f08dac6f1cd321e23671a3ca1d2f33658
Dmitry Barkov <barkvo@gmail.com>
2022-05-17 23:53:10 +0400

    FE: UI library; form template & styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -21,9 +36,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NzFormModule,
+    NzDatePickerModule,
+    NzInputNumberModule,
+    NzInputModule,
+    NzSelectModule,
+    NzButtonModule,
+    NzTypographyModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import countryList from './country-list.json';
+
+interface CountryListItem {
+  id: string;
+  label: string;
+}
+
+const buildCountryList = (i: { [key: string]: string }): ReadonlyArray<CountryListItem> =>
+  Object.entries(i).map(([key, value]) => ({ id: key, label: value }));
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +18,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  public form: FormGroup;
+
+  public countryList: ReadonlyArray<CountryListItem> = buildCountryList(countryList);
+
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.form = new FormGroup({
+      checkInOutDates: new FormControl(null, [Validators.required]),
+      guestsAmount: new FormControl(null, [Validators.required]),
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      country: new FormControl(null, [Validators.required]),
+      city: new FormControl(null, [Validators.required]),
+      address: new FormControl(null, [Validators.required]),
+      postalCode: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      phone: new FormControl(null, [Validators.required]),
+    });
   }
+
+  public submitForm() {}
 
 }
